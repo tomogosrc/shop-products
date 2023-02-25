860efc21dde9e9cc6cf73511e1c9c3e9df946eb8
tomogosrc <thomas.escudero@outlook.fr>
2023-02-25 23:27:30 +0100

    initial commit UI

---
+++ ShopProducts.UI/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localeEn from '@angular/common/locales/en';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from 'app/app-routing.module';
+import { AppComponent } from 'app/app.component';
+import { BaseModule } from 'app/base/base.module';
+import { SharedModule } from 'app/shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    BaseModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor() {
+    registerLocaleData(localeEn, 'en');
+  }
+}
